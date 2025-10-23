92f0c92880d883d763699f0d2d165af4689a4d10
Imran Rahman <imran@bglobal.com>
2025-10-24 03:04:19 +0600

    add currect external url.

--- app/Http/Controllers/Api/EightballProxyController.php
+++ app/Http/Controllers/Api/EightballProxyController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Log;
 class EightballProxyController extends Controller
 {
     private const API_KEY = 'lp_n1k6BVf3h7JRyjXkWMSoXi0BBZYRaOLL4QohDPQJrrrrraffdf';
-    private $externalApiUrl = 'http://wp-latepoint.local/wp-json/v1';
+    private $externalApiUrl = 'https://8ball.tires/wp-json/v1';
 
     /**
      * Get all locations (proxy for /locations)
